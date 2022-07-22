fa77ff954662c7518dda8c17505fe713b021c5a8
CHaOSFrozen <khansakin255@gmail.com>
2022-07-22 23:28:06 +0800

    Process Dictionary & Print Data

--- project_group/api.py
+++ project_group/api.py
@@ -1,6 +1,10 @@
 import requests
 
-url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_symbol=USD&to_symbol=SGD&apikey=D3A1NMWTIU3JYJLL'
+
+api_key = "CX1CI0CXHQRFJXP6"
+
+url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}'
 r = requests.get(url)
 data = r.json()
+print(r)
 print(data)
