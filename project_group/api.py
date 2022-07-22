0f17110045b357359ff4f9cd5332bd8f0cc2e248
CHaOSFrozen <khansakin255@gmail.com>
2022-07-23 00:50:28 +0800

    modified:   project_group/api.py

--- project_group/api.py
+++ project_group/api.py
@@ -3,7 +3,7 @@ import requests
 
 api_key = "CX1CI0CXHQRFJXP6"
 
-url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}'
+url = 'https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey={api_key}'
 r = requests.get(url)
 data = r.json()
 print(r)
