c2e3f83c17ce39a0c41d685dc0880fc064e56d5b
CHaOSFrozen <khansakin255@gmail.com>
2022-07-23 00:44:10 +0800

    modified:   project_group/cash_on_hand.py

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -5,11 +5,14 @@ import re, csv
 file_path = Path.cwd()/"project_group"/"csv_reports"/"Cash On Hand.CSV"
 print(file_path.exists())
 
+
 for file in file_path.open():
-         empty_list = []
-         with file_path.open(mode = "r", encoding = "UTF-8", newline = "") as file:
-             # instantiate a read object
-              reader = file.read()
-              # use `next()` to skip the header.
-              next(reader)
+    empty_list = [] 
+    with file_path.open(mode = "r", encoding = "UTF-8", newline = "") as file:
+        # instantiate a read object
+        reader = file.read()
+        # use `next()` to skip the header.
+        next(reader)
+        
+
               
