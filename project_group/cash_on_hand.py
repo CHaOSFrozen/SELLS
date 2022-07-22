f35624542a2efab8aea04b8dd9cf316af2d4549b
CHaOSFrozen <khansakin255@gmail.com>
2022-07-23 00:29:22 +0800

    modified:   project_group/cash_on_hand.py 	renamed:    project_group/csv_reports/Cash_On_Hand.csv -> project_group/csv_reports/Cash On Hand.csv

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -2,12 +2,12 @@ from pathlib import Path
 import re, csv
 
 
-file_path = Path.cwd()/"project_group"/"csv_reports"/"Cash_On_Hand.CSV"
+file_path = Path.cwd()/"project_group"/"csv_reports"/"Cash On Hand.CSV"
 print(file_path.exists())
 
 for file in file_path.open():
          empty_list = []
          with file_path.open(mode = "r", encoding = "UTF-8", newline = "") as file:
               reader = file.read()
-              
+
               
