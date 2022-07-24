782b7af49f23f097377f981151b1bfe7e0b68092
CHaOSFrozen <khansakin255@gmail.com>
2022-07-24 18:56:18 +0800

    modified:   project_group/api.py

--- project_group/api.py
+++ project_group/api.py
@@ -1,14 +1,14 @@
+from email import message
 import requests
 
 
 api_key = "CX1CI0CXHQRFJXP6"
 
 # Group Assingment According to PFP Project Breif 
-url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}'
-r = requests.get(url)
-data = r.json()
-print(r)
-print(data)
+# url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}'
+# r = requests.get(url)
+# data = r.json()
+# print(data)
 
 
 
@@ -16,5 +16,8 @@ print(data)
 url = 'https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey={api_key}'
 r = requests.get(url)
 data = r.json()
-print(r)
-print(data)
+
+# print(data.keys()) which 
+# prints the dictionary's keys: dict_keys(['Meta Data', 'Time Series FX (Weekly)']) 
+
+# items() method returns a view object that displays a list of dictionary's (key, value) tuple pairs.
